f2a72f68057c90f293d9e68d97d0e3a5d3260472
Your Name <you@example.com>
2018-03-16 14:04:05 -0600

    copy dictionary

--- utils.py
+++ utils.py
@@ -2,18 +2,20 @@ from data.users import *
 
 def get_user_by_id(id_user):
     if id_user in USERS:
-        tmp_dic = USERS[id_user]
+        tmp_dic = USERS[id_user].copy()
         tmp_dic["id"] = id_user
+        tmp_dic["phone"] = tmp_dic["phone"][-10:]
         return tmp_dic
     return {}
 
 
 def get_user_by_phone(phone):
     #Only work with last 10 digits
-    this_phone = phone[-10:]
+    this_phone = phone.replace(" ","")[-10:]
     for key in USERS.keys():
         if USERS[key]["phone"] == "+52"+this_phone:
-            tmp_dic = USERS[key]
+            tmp_dic = USERS[key].copy()
             tmp_dic["id"] = key
+            tmp_dic["phone"] = tmp_dic["phone"][-10:]
             return tmp_dic
     return {}
